d3e924c0cafb813954b51414e03924b4cc1fbdcf
antonio <antoniobesen@hotmail.com>
2019-11-14 14:54:51 -0300

    Criacao de novos componentes reutilizaveis

--- src/components/companyActions.vue
+++ src/components/companyActions.vue
@@ -1,51 +1,32 @@
 <template>
-  <v-container>
-    <v-row>
-      <v-col lg="6" justify="space-around">
-        <h4>Visualizar ações por empresa</h4>
-      </v-col>
-    </v-row>
-    <v-row>
-      <v-col lg="6">
-        <v-select
-          label="Empresas"
-          :items="companies"
-          item-text="name"
-          item-value="id"
-          v-model="selectedCompany"
-        ></v-select>
-      </v-col>
-      <v-col>
-        <v-btn @click="searchCompanyActions = !searchCompanyActions">Pesquisar</v-btn>
-      </v-col>
-    </v-row>
-    <v-row>
-      <v-col lg="12">
-        <v-list v-show="searchCompanyActions">
-          <v-list-item v-for="(event, index) in getCompanyEventsById(selectedCompany)" :key="index">
-            <v-list-item-content>
-              <v-list-item-title v-text="event.name"></v-list-item-title>
-            </v-list-item-content>
-          </v-list-item>
-        </v-list>
-      </v-col>
-    </v-row>
-  </v-container>
+  <company-actions-card title="Visualizar ações por empresa">
+    <template v-slot:body="{ selectedCompany }">
+      <v-list v-show="selectedCompany">
+        <v-list-item v-for="(event, index) in getCompanyEventsById(selectedCompany)" :key="index">
+          <v-list-item-content>
+            <v-list-item-title v-text="event.name"></v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </template>
+  </company-actions-card>
 </template>
 
 
 <script>
-import { mapState, mapGetters } from "vuex";
+import { mapGetters } from "vuex";
+import CompanyActionsCard from "@/components/CompanyActionsCard.vue";
 
 export default {
+  components: {
+    CompanyActionsCard
+  },
   data() {
     return {
-      selectedCompany: 0,
       searchCompanyActions: false
     };
   },
   computed: {
-    ...mapState(["companies"]),
     ...mapGetters(["getCompanyEventsById"])
   }
 };
---
+++ src/components/form/CompanySelect.vue
@@ -0,0 +1,35 @@
+<template>
+  <v-select
+    label="Empresas"
+    v-bind="$attrs"
+    :items="companies"
+    item-text="name"
+    item-value="id"
+    v-model="selectedCompany"
+  ></v-select>
+</template>
+
+<script>
+import { mapState } from "vuex";
+export default {
+  props: {
+    value: {
+      type: Object,
+      required: true
+    }
+  },
+  data() {
+    return {
+      selectedCompany: { ...this.value }
+    };
+  },
+  computed: {
+    ...mapState(["companies"])
+  },
+  watch: {
+    selectedCompany() {
+      this.$emit("input", this.selectedCompany);
+    }
+  }
+};
+</script>
